ac675de788393fc7ae1b31b5883d416abc724d58
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-02-05 17:36:59 +0000

    Add automatic product definition creation

--- src/stac_to_dc/domain/operations.py
+++ src/stac_to_dc/domain/operations.py
@@ -1,5 +1,8 @@
 import os
+from pathlib import Path
 from typing import Tuple
+
+from datacube import index, model
 
 from stac_to_dc.util import load_json
 
@@ -36,3 +39,29 @@ def get_item(stac_item: str) -> Tuple[dict, str, bool]:
     metadata = load_json(stac_item)
     uri, relative = guess_location(metadata)
     return metadata, uri, relative
+
+
+def get_collection_url(stac_item: str) -> str:
+    metadata = load_json(stac_item)
+    for link in metadata.get("links"):
+        rel = link.get("rel")
+        if rel and rel == "collection":
+            return str((Path(stac_item).parent / link.get("href")).absolute())
+
+
+def get_product_definition(collection_url: str) -> dict:
+    collection = load_json(collection_url)
+    if collection.get("product_definition"):
+        product_definition = {
+            "name": collection.get("id"),
+            "description": collection.get("description"),
+            "metadata_type": collection.get("product_definition").get("metadata_type"),
+            "metadata": collection.get("product_definition").get("metadata"),
+            "measurements": collection.get("product_definition").get("measurements")
+        }
+
+        return product_definition
+
+
+def create_product(dc_index: index.Index, product_definition: dict) -> model.DatasetType:
+    return dc_index.products.from_doc(product_definition)
--- test/unit/test_operations.py
+++ test/unit/test_operations.py
@@ -1,4 +1,6 @@
-from stac_to_dc.domain.operations import get_item, guess_location
+from datacube import Datacube
+from stac_to_dc.domain.operations import get_item, guess_location, get_product_definition, create_product, \
+    get_collection_url
 from stac_to_dc.util import load_json
 
 
@@ -19,3 +21,27 @@ def test_guess_location_relative():
                          'UKSA_SPOT155_SO18034609-55-01_DS_SPOT6_201810101058095_FR1_FR1_FR1_FR1_W001N52_01140.json')
     uri, relative = guess_location(metadata)
     assert relative
+
+
+def test_get_collection_url():
+    collection_url = get_collection_url('test/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
+    assert collection_url
+
+
+def test_get_product_definition():
+    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    assert product_definition
+
+
+def test_create_product():
+    dc = Datacube()
+    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    product = create_product(dc.index, product_definition)
+    assert product
+
+
+def test_add_product():
+    dc = Datacube()
+    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    product = create_product(dc.index, product_definition)
+    dc.index.products.add(product)
