e911280360e36ebf7e2473435bd1793c9330fe3a
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-02-08 18:22:09 +0000

    Refactoring test folder to tests

--- tests/unit/test_operations.py
+++ tests/unit/test_operations.py
@@ -7,18 +7,18 @@ from stac_to_dc.util import load_json
 
 
 def test_get_item():
-    item = get_item('test/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
+    item = get_item('tests/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
     assert len(item) == 3
 
 
 def test_guess_location_absolute():
-    metadata = load_json('test/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
+    metadata = load_json('tests/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
     uri, relative = guess_location(metadata)
     assert not relative
 
 
 def test_guess_location_relative():
-    metadata = load_json('test/data/uksa-ssgp/uksa-ssgp-spot/'
+    metadata = load_json('tests/data/uksa-ssgp/uksa-ssgp-spot/'
                          'UKSA_SPOT155_SO18034609-55-01_DS_SPOT6_201810101058095_FR1_FR1_FR1_FR1_W001N52_01140/'
                          'UKSA_SPOT155_SO18034609-55-01_DS_SPOT6_201810101058095_FR1_FR1_FR1_FR1_W001N52_01140.json')
     uri, relative = guess_location(metadata)
@@ -26,29 +26,29 @@ def test_guess_location_relative():
 
 
 def test_get_collection_url_same_level():
-    collection_url = get_collection_url('test/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
-    assert Path(collection_url) == Path('./test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json').absolute()
+    collection_url = get_collection_url('tests/data/sentinel-s2-l2a-cogs/S2A_30VXL_20210203_0_L2A.json')
+    assert Path(collection_url) == Path('./tests/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json').absolute()
 
 
 def test_get_collection_url_parent_level():
-    collection_url = get_collection_url('test/data/sentinel-s2-l2a-cogs/S2A_30VXL/S2A_30VXL_20210203_0_L2A.json')
-    assert Path(collection_url) == Path('./test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json').absolute()
+    collection_url = get_collection_url('tests/data/sentinel-s2-l2a-cogs/S2A_30VXL/S2A_30VXL_20210203_0_L2A.json')
+    assert Path(collection_url) == Path('./tests/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json').absolute()
 
 
 def test_get_product_definition():
-    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    product_definition = get_product_definition('tests/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
     assert product_definition
 
 
 def test_create_product():
     dc = Datacube()
-    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    product_definition = get_product_definition('tests/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
     product = create_product(dc.index, product_definition)
     assert product
 
 
 def test_add_product():
     dc = Datacube()
-    product_definition = get_product_definition('test/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
+    product_definition = get_product_definition('tests/data/sentinel-s2-l2a-cogs/sentinel-s2-l2a-cogs.json')
     product = create_product(dc.index, product_definition)
     dc.index.products.add(product)
