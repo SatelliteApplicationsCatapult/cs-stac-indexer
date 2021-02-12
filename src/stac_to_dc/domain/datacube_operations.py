037fcdc4620825bac76c7247a5f2811ef208eabd
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-02-12 14:42:29 +0000

    Move datacube_operations to application layer

--- src/stac_to_dc/domain/datacube_operations.py
+++
@@ -1,5 +0,0 @@
-from datacube import index, model
-
-
-def create_product(dc_index: index.Index, product_definition: dict) -> model.DatasetType:
-    return dc_index.products.from_doc(product_definition)
