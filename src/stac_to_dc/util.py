23866634854466af24ce62dc70c0c57a5fad0945
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-02-18 18:04:59 +0000

    Add parse_s3_link and get_rel_links util functions

--- src/stac_to_dc/util.py
+++ src/stac_to_dc/util.py
@@ -1,6 +1,7 @@
 import json
-from typing import List
+from typing import List, Tuple
 from pathlib import Path
+from urllib.parse import urlparse
 
 
 def load_json(file: str) -> dict:
@@ -10,3 +11,11 @@ def load_json(file: str) -> dict:
 
 def get_files_from_dir(directory: str, extension: str) -> List[str]:
     return [str(x.absolute()) for x in Path(directory).glob(f'**/*.{extension}')]
+
+
+def parse_s3_link(link: str) -> Tuple[str, str]:
+    return urlparse(link).path.split('/')[1], '/'.join(urlparse(link).path.split('/')[2:])
+
+
+def get_rel_links(metadata: dict, rel: str) -> List[str]:
+    return [link.get('href') for link in metadata.get('links') if link.get('rel') == rel]
--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -1,6 +1,50 @@
-from stac_to_dc.util import get_files_from_dir
+from stac_to_dc.util import get_files_from_dir, parse_s3_link, load_json, get_rel_links
 
 
 def test_get_files_from_dir():
     files = get_files_from_dir('tests/data/uksa-ssgp', 'json')
     assert files
+
+
+def test_parse_s3_link():
+    link = "https://s3-uk-1.sa-catapult.co.uk/public-eo-data/cs_stac/sentinel-2/collection.json"
+    bucket, key = parse_s3_link(link=link)
+    assert (bucket, key) == ('public-eo-data', 'cs_stac/sentinel-2/collection.json')
+
+
+def test_rel_links_child():
+    metadata = load_json('tests/data/catalog.json')
+    child_links = get_rel_links(metadata, 'child')
+    assert child_links == ["https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2"
+                           "/collection.json"]
+
+
+def test_rel_links_item():
+    metadata = load_json('tests/data/sentinel-2/collection.json')
+    child_links = get_rel_links(metadata, 'item')
+    assert child_links == ['https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151002T222056_T01KAU/S2A_MSIL2A_20151002T222056_T01KAU.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151002T222056_T01KBU/S2A_MSIL2A_20151002T222056_T01KBU.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151022T222102_T01KAU/S2A_MSIL2A_20151022T222102_T01KAU.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151022T222102_T01KBU/S2A_MSIL2A_20151022T222102_T01KBU.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KWD/S2A_MSIL2A_20151114T223002_T60KWD.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KWE/S2A_MSIL2A_20151114T223002_T60KWE.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KWF/S2A_MSIL2A_20151114T223002_T60KWF.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KWG/S2A_MSIL2A_20151114T223002_T60KWG.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KXD/S2A_MSIL2A_20151114T223002_T60KXD.json',
+                           'https://s3-uk-1.sa-catapult.co.uk/public-eo-data/stac_catalogs/cs_stac/sentinel-2'
+                           '/S2A_MSIL2A_20151114T223002_T60KXE/S2A_MSIL2A_20151114T223002_T60KXE.json']
+
+
+def test_rel_links_no_exists():
+    metadata = load_json('tests/data/catalog.json')
+    child_links = get_rel_links(metadata, 'foo')
+    assert child_links == []
