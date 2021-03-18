394c20d6c39ed1434ae3518350f92c51b3d1ba9a
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-03-18 15:34:40 +0000

    Add entrypoint to package

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,9 @@ setup(
     name='stac_to_dc',
     version='0.1',
     packages=['stac_to_dc'],
+    entry_points={
+        'console_scripts': [
+            'stac-to-dc=stac_to_dc.cli:main',
+        ],
+    }
 )
