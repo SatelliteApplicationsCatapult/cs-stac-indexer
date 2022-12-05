6b3423de744006b24abba54b8790aa82eb0304f3
James <james.hinton@live.co.uk>
2022-12-05 13:41:57 +0000

    ported requirements to setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,34 @@
 from setuptools import setup
 
+extras_require = {
+    # These are all optional but nice to have on a real deployment
+    "deployment": [
+        # Performance
+        "ciso8601",
+        "bottleneck",
+        # The default run.sh and docs use gunicorn+meinheld
+        "gunicorn",
+        "setproctitle",
+        "meinheld",
+        "gevent",
+        # Monitoring
+        "raven",
+        "blinker",
+        "prometheus-flask-exporter",
+    ],
+}
+
 setup(
     name='stac_to_dc',
     version='0.1',
-    packages=['stac_to_dc', 'cubedash'],
+    packages=['stac_to_dc'],
     setup_requires=["setuptools_scm"],
     install_requires=[
         "cachetools",
         "click",
         "dataclasses>=0.6;python_version<'3.7'",
         "datacube>=1.8",
+        "datacube-explorer",
         "eodatasets3 >= 0.17.0",
         "fiona",
         "Flask==2.2.2",
@@ -29,6 +48,7 @@ setup(
         "PyYAML>=5.3.1",
         "asyncio-nats-client==0.11.5",
     ],
+    extras_require=extras_require,
     entry_points={
         'console_scripts': [
             'stac-to-dc=stac_to_dc.cli:main',
